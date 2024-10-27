106b73c92ed276e55a68e2bf83b8335164446842
Shivam Pandey <shivampandey@Shivams-MacBook-Air.local>
2024-10-28 02:06:23 +0530

    first commit

---
+++ src/components/signup/SignUp.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import './signUp.css'
+
+import {BrowserRouter as Router, Routes, Route} from 'react-router-dom'
+
+const SignUp = () => {
+  return (
+    
+      <Routes>
+        {/* <Route path = "/" element={<Occupation />} /> */}
+
+      </Routes>
+    
+  )
+}
+
+export default SignUp
