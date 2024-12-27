16610c584cd66b014da776e2e0ac5a8fdfbe346e
Shivam Pandey <shivampandey01032003@gmail.com>
2024-12-27 12:47:48 +0530

    package json update

--- src/components/signup/SignUp.jsx
+++ src/components/signup/SignUp.jsx
@@ -1,16 +1,8 @@
 import React from 'react'
-import './signUp.css'
-
-import {BrowserRouter as Router, Routes, Route} from 'react-router-dom'
 
 const SignUp = () => {
   return (
-    
-      <Routes>
-        {/* <Route path = "/" element={<Occupation />} /> */}
-
-      </Routes>
-    
+    <div>SignUp</div>
   )
 }
 
